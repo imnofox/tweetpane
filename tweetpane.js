df5636e64dc31c78e5b4e1847079affab000ca3e
imnofox <lachlan@imnofox.com>
2017-08-14 20:24:19 +1200

    Initial commit

---
+++ tweetpane.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  if ($('li.home.active').length > 0) {
+    $('.tweetpane').remove();
+    var $frame = $('<iframe>');
+    $frame.addClass('tweetpane');
+    $frame.attr('src', "https://twitter.com/i/notifications/?tweetpane=1");
+
+    $('.dashboard-right').prepend($frame);
+  }
+});
